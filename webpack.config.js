2bf13a8b5d10eef70c9a9a43fac6846532a86a4e
unknown <twomeaningsof@gmail.com>
2022-01-12 21:12:11 +0100

    Add project structure and setup

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: isProduction ? "production" : "development",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|ico)$/i,
+        type: "asset",
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+    new FaviconsWebpackPlugin("./src/assets/img/favicon.ico"),
+  ],
+  devServer: {
+    open: true,
+    host: "localhost",
+  },
+};
