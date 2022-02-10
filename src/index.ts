a0086ba07568565acff833b56b19118122e92e42
unknown <twomeaningsof@gmail.com>
2022-02-11 00:31:27 +0100

    Handle resolutions for < 1025px width.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,18 @@ import { LandingPresenter } from "./app/presenter/Landing";
 import { SettingsModel } from "./app/model/Settings";
 
 window.onload = () => {
+  const mobileMessageWrapper = document.createElement("div");
+  mobileMessageWrapper.classList.add("mobile-message-wrapper");
+
+  const mobileMessage = document.createElement("div");
+  mobileMessage.classList.add("mobile-message-wrapper__message");
+  mobileMessage.textContent =
+    "Fantasy quiz app supports only desktops, i.e. +1024px width windows";
+
+  mobileMessageWrapper.appendChild(mobileMessage);
+
+  document.body.appendChild(mobileMessageWrapper);
+
   const settingsModel = new SettingsModel();
   const landingPresenter = new LandingPresenter(settingsModel);
   landingPresenter.initialize();
